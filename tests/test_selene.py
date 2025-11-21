cdbfe364a474b2365cc08d9b52298d23523ee10b
Darya Belevtseva <dascha067@gmail.com>
2025-11-21 22:41:05 +0300

    Выполнила Чистый Selene (без шагов)

--- tests/test_selene.py
+++ tests/test_selene.py
@@ -1,18 +1,4 @@
 from selene import browser, have
-import pytest
-
-#
-# def test_issue_title_clean_selene():
-#     browser.open('https://github.com/dascha067-prog/QA_GURU_BELEVTSEVA_HW_5')
-#
-#     # Открываю Issues
-#     browser.element('#issues-tab').should(be.clickable).click()
-#
-#     # Ждём, пока появится Issue #3 в списке (Test issue B)
-#     browser.all('a.Link--primary').element_by(have.text('Test issue B')).click()
-#
-#     # Проверяю заголовок
-#     browser.element('span.js-issue-title').should(have.text('Test issue B'))
 
 
 def test_issue_title_is_visible():
